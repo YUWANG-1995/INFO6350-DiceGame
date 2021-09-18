d3319f9f7d8da2f1604ab124741afb7a37015563
YuWang <wang.yu14@northeastern.edu>
2021-09-17 20:08:09 -0700

    Initial Commit

---
+++ DiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by Yu Wang on 2021/9/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
